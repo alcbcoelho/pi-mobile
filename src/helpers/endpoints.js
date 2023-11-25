865d751b79eede948d1aca8e828d2fdeaca7aa1d
Stefany Ferreira da Silva <stefanysferreira18@gmail.com>
2023-11-25 12:35:06 -0300

    criando arquivo auxiliar que traça os endpoints de integração com o back closed #32

---
+++ src/helpers/endpoints.js
@@ -0,0 +1,21 @@
+const endpoints = {
+	BASE_URL: 'http://localhost:3000',
+	SIGNIN_URL: '/signin',
+	SIGNOUT_URL: '/signout',
+	SIGNUP_URL: '/signup',
+	PUBLIC_URL: '/public',
+	REFRESH_URL: '/refresh',
+	RESET_URL: '/reset',
+
+	USERS_URL: '/users',
+	USERS_UPLOAD_URL: '/users/upload',
+
+	ITEMS_URL: '/items',
+	ITEMS_ID_URL: '/items/',
+	ITEMS_UPLOAD_ID_URL: '/items/upload/',
+
+	MATCHES_URL: '/matches',
+	MATCHES_ID_URL: '/matches/',
+};
+
+export default endpoints;
